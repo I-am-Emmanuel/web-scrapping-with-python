295a98e770e85017c30ef27e83521f08ea1a27cd
hemakulate28 <olaifaemmanueloluwatobiloba@gmail.com>
2023-08-20 00:30:19 +0100

    using beautifulsoup

--- scrape.py
+++ scrape.py
@@ -3,5 +3,7 @@ from bs4 import BeautifulSoup
 
 response = requests.get('https://news.ycombinator.com/news')
 soupobj = BeautifulSoup(response.text, 'html.parser')
-print(soupobj.find_all('div'))
-print(soupobj.find('a'))
+# print(soupobj.find_all('div'))
+# print(soupobj.find('a'))
+# print(soupobj.select('.score'))
+print(soupobj.select(''))
