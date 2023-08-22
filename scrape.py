fdaddb8d6af1bec5b90029a6e53c848a66597801
hemakulate28 <olaifaemmanueloluwatobiloba@gmail.com>
2023-08-22 02:31:18 +0100

    creating custom hack and news2

--- scrape.py
+++ scrape.py
@@ -1,22 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 response = requests.get('https://news.ycombinator.com/news')
 soupobj = BeautifulSoup(response.text, 'html.parser')
 link = soupobj.select('.titleline')
 vote = soupobj.select('.score')
+subtext = soupobject.select('.subtext')
 
-def create_custome_hn(links, votes):
+# def create_custome_hn(links, votes):
+#     hack_news = []
+#     for index, item in enumerate(links):
+#         points = int(votes[index].getText().replace(' points', ''))
+#         if points > 99:
+#             title = links[index].getText()
+#             href = links[index].get('href', None)
+#             hack_news.append({'title': title, 'href': href})
+
+#     return hack_news
+
+def create_custome_hackn(links, subtext):
     hack_news = []
     for index, item in enumerate(links):
-        title = links[index].getText()
-        href = links[index].get('href', None)
-        hack_news.append({'title': title, 'href': href})
+        title = item[index].getText()
+        href = item[index].get('href', None)
+        vote = subtext[index].select('.score')
+        if len(vote):
+            point = int(vote[0].getText().replace(' points', ''))
+            if point > 99:
+                hack_news.append({'title': title, 'href': href, 'votes': point})
 
     return hack_news
+ 
 
-
-print(create_custome_hn(links=link, votes = vote))
+pprint.pprint(create_custome_hn(links=link, votes = subtext))
 
 
 # print(hack_news)
