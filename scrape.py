10966f1dfe489ca56b76bdc22b6fa003160ef431
hemakulate28 <olaifaemmanueloluwatobiloba@gmail.com>
2023-08-22 01:35:12 +0100

    css selectors contd

--- scrape.py
+++ scrape.py
@@ -3,8 +3,15 @@ from bs4 import BeautifulSoup
 
 response = requests.get('https://news.ycombinator.com/news')
 soupobj = BeautifulSoup(response.text, 'html.parser')
+links = soupobj.select('.titleline')
+votes = soupobj.select('.score')
+
+print(links)
+
+
+
 # print(soupobj.find_all('div'))
 # print(soupobj.find('a'))
-print(soupobj.body.contents)
+# print(soupobj.body.contents)
 # print(soupobj.select('.score'))
 # print(soupobj.select(''))
